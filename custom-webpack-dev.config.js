d7cbba0ad26b03b9f4c8d24e2da4665a247a2de3
Budi Irawan <deerawan@gmail.com>
2020-03-28 19:13:53 +1100

    chore: setup chrome extension config

---
+++ custom-webpack-dev.config.js
@@ -0,0 +1,12 @@
+const ExtensionReloader = require('webpack-extension-reloader')
+const config = require('./custom-webpack.config');
+
+module.exports = {...config,
+    mode: 'development',
+    plugins: [new ExtensionReloader({
+        reloadPage: true,
+        entries: {
+            background: 'background'
+        }
+    })]
+}
