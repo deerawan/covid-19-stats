08f53d4e611e2a69997c944259dcbaf40622bd9b
Budi Irawan <deerawan@gmail.com>
2020-04-10 11:51:56 +1000

    feat: add e2e test

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,5 +1,42 @@
-it("loads examples", () => {
-  const baseUrl: string = "http://localhost:4200";
+before(() => {
+  const baseUrl = 'http://localhost:4200';
   cy.visit(baseUrl);
-  cy.contains("Replace me with something relevant");
+});
+
+it('displays global stats', () => {
+  cy.contains('Global');
+  cy.contains('Total cases');
+  cy.contains('New cases');
+  cy.contains('Total deaths');
+  cy.contains('New deaths');
+  cy.contains('Total recovered');
+  cy.contains('Critical');
+  cy.contains('Cases / Million');
+  cy.contains('Deaths / Million');
+});
+
+it('displays country stats when a country is selected', () => {
+  cy.contains('Select country');
+  cy.get('[data-test-id="country"]').type('Australia').type('{enter}');
+
+  cy.contains('Australia');
+});
+
+it('back to global stat when country select is cleared', () => {
+  cy.get('.ng-clear-wrapper') // vendor lib, couldn't add data-test-id as target
+    .click();
+
+  cy.contains('Global');
+});
+
+it('could switch to dark mode', () => {
+  cy.get('[data-test-id="mode-knob"]').click();
+
+  cy.get('body').should('have.class', 'theme-dark');
+});
+
+it('could switch back to light mode', () => {
+  cy.get('[data-test-id="mode-knob"]').click();
+
+  cy.get('body').should('not.have.class', 'theme-dark');
 });
