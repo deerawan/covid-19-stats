ce46853b98cb4ec362dc1930585b7d5370a0e8cb
Budi Irawan <deerawan@gmail.com>
2020-04-15 21:11:08 +1000

    using chrome as target browser

--- src/app/corona.service.ts
+++ src/app/corona.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { GlobalStat, CountryStat } from './api.model';
 
+const BASE_URL = 'https://corona.lmao.ninja';
 @Injectable({
   providedIn: 'root'
 })
@@ -10,12 +11,10 @@ export class CoronaService {
   constructor(private httpClient: HttpClient) {}
 
   getGlobalStat(): Observable<GlobalStat> {
-    return this.httpClient.get<GlobalStat>('https://corona.lmao.ninja/all');
+    return this.httpClient.get<GlobalStat>(`${BASE_URL}/all`);
   }
 
   getCountriesStat(): Observable<CountryStat[]> {
-    return this.httpClient.get<CountryStat[]>(
-      'https://corona.lmao.ninja/countries'
-    );
+    return this.httpClient.get<CountryStat[]>(`${BASE_URL}/countries`);
   }
 }
